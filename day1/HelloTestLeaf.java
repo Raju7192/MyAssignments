4bbadfa2406cd96c5f0cd2fffa9f36e02f4cfd5d
Raju7192 <rajusekar62@gmail.com>
2025-01-19 02:05:37 +0530

    Add files via upload
    day1

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f;
+		String browserName = "Firfox";
+		boolean isVisible = true;
+		int releaseYear =1998;
+		char browserLogo = 'f';
+		
+		System.out.println("Browser Version: " + browserVersion);
+		System.out.println("Browser Name: " + browserName);
+		System.out.println("Browser is visible(True/False): " + isVisible);
+		System.out.println("Browser Released Year: " + releaseYear);
+		System.out.println("Browser Logo: " + browserLogo);
+	}
+}
+	
